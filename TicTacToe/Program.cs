8330f57a016a7348c47e08839930f547a71dcc26
Mitch Boling <mphboling@gmail.com>
2024-04-16 20:52:38 -0400

    Made declarations and started win/loss/draw conditions

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.CursorVisible = true;
+bool gameRunning = true;
+bool userTurn = true;
+
+while (gameRunning)
+    {
+        if (userTurn)
+        {
+            userTurn();
+            if (LookForWin('X'))
+            {
+                GameEnd("Congratulations! You win!");
+                break;
+            }
+        }
+        else {
+            AITurn();
+            if (LookForWin('O'))
+            {
+                GameEnd ("You lost :(");
+                break;
+            }
+        }
+        userTurn = !userTurn;
+        if (LookForFilledGrid())
+        {
+            GameEnd ("A strange game. The only winning move is not to play.");
+            break;
+        }
+    }
+
+
+
+
+
